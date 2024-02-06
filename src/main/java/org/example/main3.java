4b78c586f4cde308f04ad3c92e053b9186f5af8b
lostfly2 <lostfly2@naver.com>
2024-02-06 23:13:45 +0900

    프록시 관련 공부

--- src/main/java/org/example/main3.java
+++ src/main/java/org/example/main3.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.artist.domain.Movie;
 import org.example.member.domain.Member;
 import org.example.member.domain.Order;
 import org.example.member.domain.orderItem;
@@ -21,6 +22,18 @@ public class main3 {
 
         try{
 
+            Member member= new Member();
+            member.setName("gghoelo");
+            em.persist(member);
+            em.flush();
+            em.clear();
+
+            Member findmemeber = em.getReference(Member.class, member.getId());
+
+            System.out.println("findmemeber ::"+ findmemeber.getName());
+            // 프록시일때는 아예 객체만 달려있는 빈값으로 설정이 되어있다.
+            // 그래서 값을 호출할때 쿼리를 발생시킨 뒤 호출한다.
+            // 호출 이후에는 레퍼런스가 초기화 된다.
 
         }catch (Exception e){
             tx.rollback();
